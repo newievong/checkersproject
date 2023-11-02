10d1529a753cd56ab5f7c3b56ed75cb79a706a22
newievong <143970329+newievong@users.noreply.github.com>
2023-11-01 21:35:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import checkers
 def game():
-    import checkers
-
     howbig = int(input("How big would you like your checkerboard? Enter an integer between 4 and 16: "))
+    while howbig <= 4 or howbig >= 16:
+        howbig = int(input(f'{howbig} is not a valid input. Please enter a value between 4 and 16: '))
+
     board = checkers.build_board(howbig)
 
     black_count = checkers.get_count(board, 'Black')
     red_count = checkers.get_count(board, 'Red')
     empty_count = checkers.get_count(board, 'Empty')
 
+    print(board)
     print(f"Black pieces: {black_count}")
     print(f"Red pieces: {red_count}")
     print(f"Empty spaces: {empty_count}")
